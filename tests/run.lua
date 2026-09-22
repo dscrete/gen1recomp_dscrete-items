ac41567d410e0d6363b66513dc1948fb72c7a820
dwyschwikkard-alt <dwyschwikkard@gmail.com>
2026-09-22 17:27:06 +0200

    Run Silph Tracker smoke test

--- tests/run.lua
+++ tests/run.lua
@@ -28,6 +28,7 @@ dofile("tests/items_test.lua")
 dofile("tests/runtime_test.lua")
 dofile("tests/shiny_finder_test.lua")
 dofile("tests/encounter_weights_test.lua")
+dofile("tests/silph_tracker_test.lua")
 
 io.write(("%d passed, %d failed\n"):format(passed, failed))
 if failed > 0 then os.exit(1) end
