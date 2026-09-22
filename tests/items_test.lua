db88732b1f8f788884b31fc008f65fee378cf97d
dwyschwikkard-alt <dwyschwikkard@gmail.com>
2026-09-22 21:57:10 +0200

    Update implemented item catalogue tests

--- tests/items_test.lua
+++ tests/items_test.lua
@@ -20,7 +20,8 @@ end)
 test("implemented encounter consumables have effects", function()
   local expected={ prism_scent="DS_PRISM_SCENT_EFFECT", elusive_scent="DS_ELUSIVE_SCENT_EFFECT",
     mystery_lure="DS_MYSTERY_LURE_EFFECT", species_whistle="DS_SPECIES_WHISTLE_EFFECT",
-    prototype_resonator="DS_PROTOTYPE_RESONATOR_EFFECT", safari_kit="DS_SAFARI_KIT_EFFECT" }
+    prototype_resonator="DS_PROTOTYPE_RESONATOR_EFFECT", safari_kit="DS_SAFARI_KIT_EFFECT",
+    glitch_detector="DS_GLITCH_DETECTOR_EFFECT" }
   for key,effect in pairs(expected) do
     local item=Items.byKey[key]
     eq(item.effect,effect,key)
@@ -30,19 +31,21 @@ test("implemented encounter consumables have effects", function()
   end
 end)
 
-test("Silph Tracker is the only implemented permanent gadget", function()
+test("implemented permanent gadgets are tracker and treasure detector", function()
   local gadgets=Items.permanent(true)
-  eq(#gadgets,1)
+  eq(#gadgets,2)
   eq(gadgets[1].key,"silph_tracker")
+  eq(gadgets[2].key,"treasure_detector")
 end)
 
 test("implemented filters expose only built content", function()
   local consumables=Items.consumables(true)
-  eq(#consumables,6)
+  eq(#consumables,7)
   eq(consumables[1].key,"prism_scent")
   eq(consumables[2].key,"elusive_scent")
   eq(consumables[3].key,"mystery_lure")
   eq(consumables[4].key,"species_whistle")
   eq(consumables[5].key,"prototype_resonator")
   eq(consumables[6].key,"safari_kit")
+  eq(consumables[7].key,"glitch_detector")
 end)
