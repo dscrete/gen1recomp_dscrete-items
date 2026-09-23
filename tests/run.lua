48dea0708eb0cd48e9ca013f3d420f58e64df9f5
dscrete <dwyschwikkard@gmail.com>
2026-09-23 22:23:09 +0200

    Run Trainer Beacon and Link Cable tests

--- tests/run.lua
+++ tests/run.lua
@@ -35,6 +35,7 @@ dofile("tests/detectors_test.lua")
 dofile("tests/pokedex_chip_test.lua")
 dofile("tests/rocket_decoder_test.lua")
 dofile("tests/battle_tools_test.lua")
+dofile("tests/trainer_beacon_link_cable_test.lua")
 
 io.write(("%d passed, %d failed\n"):format(passed, failed))
 if failed > 0 then os.exit(1) end
